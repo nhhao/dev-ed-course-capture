d67b5f8c86ef1da340ed5c0c51da57d8dd96efac
nhhao <18110102@student.hcmute.edu.vn>
2020-12-09 06:34:52 +0700

    Style About Us

---
+++ src/components/AboutSection.js
@@ -0,0 +1,34 @@
+import home1 from '../img/home1.png';
+import { About, Description, Hide, Image } from '../styles';
+
+const AboutSection = () => {
+    return (
+        <About>
+            <Description>
+                <div className="title">
+                    <Hide>
+                        <h2>We work to make</h2>
+                    </Hide>
+                    <Hide>
+                        <h2>
+                            your <span>dreams</span> come
+                        </h2>
+                    </Hide>
+                    <Hide>
+                        <h2>true.</h2>
+                    </Hide>
+                </div>
+                <p>
+                    Contact us for any photography or videography idea that you
+                    have. We have professionals with amazing skills
+                </p>
+                <button>Contact Us</button>
+            </Description>
+            <Image className="image">
+                <img src={home1} alt="" />
+            </Image>
+        </About>
+    );
+};
+
+export default AboutSection;
