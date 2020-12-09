13616ed42f0102f136236b82fa7f0222f56f43e7
nhhao <18110102@student.hcmute.edu.vn>
2020-12-09 07:21:41 +0700

    React Route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import AboutUs from './pages/AboutUs';
 import GlobalStyle from './components/GlobalStyle';
 import Nav from './components/Nav';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
     return (
         <div className="App">
             <GlobalStyle />
             <Nav />
-            <AboutUs />
+            <Switch>
+                <Route path="/" exact>
+                    <AboutUs />
+                </Route>
+                <Route path="/work">
+                    <OurWork />
+                </Route>
+                <Route path="/contact">
+                    <ContactUs />
+                </Route>
+            </Switch>
         </div>
     );
 }
